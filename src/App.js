5a48429b273347dab7d4721d3f7efa0c4fdaf02c
Sumit Agrawal <sumit@corestrat.ai>
2024-04-10 15:33:27 +0530

    rating_final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
- import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/*Accordina Component */}
-      <Accordian/>
+      <Accordian />
       {/*Random Color Component */}
-      <RandomColor/>
+      <RandomColor />
+      {/* Star Rating Component*/}
+      <StarRating noOfStars={5}/>
     </div>
   );
 }
