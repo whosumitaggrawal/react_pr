4e7c21b50e933f94b6af27f433fe3bb1909b66ae
Sumit Agrawal <sumit@corestrat.ai>
2024-04-10 12:35:32 +0530

    adding_single_selection

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentID) {
+    setSelected(getCurrentID === selected ? null : getCurrentID);
+  }
+  console.log(selected);
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </div>
+  );
+}
